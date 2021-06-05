9f41dda18130af21f8c81fb2003b515ca3529073
Alex Vieira <alex.vieira30@etec.sp.gov.br>
2021-06-05 18:47:37 -0300

    Footer Adjusts

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,31 +1,36 @@
+import ImgLogoStilingue from "assets/img/logotipo-stilingue-vertical.svg";
+
 const Footer = () => {
   return (
     <footer className='footer mt-auto py-3 bg-dark'>
-      <div className='container'>
-        <p className='text-light'>
-          App desenvolvido por{" "}
+      <p className='text-light'>
+        App desenvolvido por{" "}
+        <a
+          href='https://github.com/AlexPauloVieira'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Alex Paulo
+        </a>
+      </p>
+      <p className='text-light'>
+        <small>
+          <strong>Desafio Solution Engineer</strong>
+          <br />
+          Projeto desenvolvido como parte do Desafio Solution Engineer da:{" "}
           <a
-            href='https://github.com/AlexPauloVieira'
+            href='https://www.instagram.com/stilingue_ia/'
             target='_blank'
             rel='noreferrer'
           >
-            Alex Paulo
+            @stilingue_ia
           </a>
-        </p>
-        <p className='text-light'>
-          <small>
-            <strong>Desafio Solution Engineer</strong>
-            <br />
-            Projeto desenvolvido como parte do Desafio Solution Engineer da:{" "}
-            <a
-              href='https://www.instagram.com/stilingue_ia/'
-              target='_blank'
-              rel='noreferrer'
-            >
-              @stilingue_ia
-            </a>
-          </small>
-        </p>
+        </small>
+      </p>
+      <div className='my-2 my-md-0 mr-md-3'>
+        <a href='https://stilingue.com.br/' target='_blank' rel='noreferrer'>
+          <img src={ImgLogoStilingue} alt='Stilingue' width='180' />
+        </a>
       </div>
     </footer>
   );
